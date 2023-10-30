c0d0db801ac35c4695309d7beb2607727e0ae50e
Carlosdvp <carlosdvp.tx@gmail.com>
2023-10-30 19:14:10 -0400

    did some final cleanup in the getPrices function.

--- client/getPrices.ts
+++ client/getPrices.ts
@@ -13,9 +13,9 @@ async function getPricesV3(address_from: string, address_to: string, amount_in: 
   const amountIn = ethers.parseUnits(amount_in, CurrentConfig.tokens.in.decimals)
 
   const quoteAmountOut = await quoterContract.quoteExactInputSingle.staticCall(
-    constant.USDC_CONTRACT,
-    constant.WETH_CONTRACT,
-    3000,
+    address_from,
+    address_to,
+    CurrentConfig.tokens.poolFee,
     amountIn.toString(),
     0
   )
@@ -28,9 +28,14 @@ async function getPricesV3(address_from: string, address_to: string, amount_in: 
   return outputAmount;
 }
 
-const main = async () => {
-  const amount_in = '100';
-  const amountOut: string = await getPricesV3(constant.USDC_CONTRACT, constant.WETH_CONTRACT, amount_in);
+const main = async (): Promise<void> => {
+  const amount_in: string = '100';
+
+  const amountOut: string = await getPricesV3(
+    CurrentConfig.tokens.in.address,
+    CurrentConfig.tokens.out.address,
+    amount_in
+  );
 
   console.log('amount out: ', amountOut.slice(0, 8))
 }
